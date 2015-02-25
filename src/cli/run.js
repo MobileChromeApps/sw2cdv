f20d120baefa346e2052a0e1b8c45da51d7e5566
Michal Mocny <mmocny@gmail.com>
2015-02-25 10:51:27 -0500

    Update to serve without ssl but from localhost so SW can work locally

--- src/cli/run.js
+++ src/cli/run.js
@@ -3,61 +3,12 @@
 /******************************************************************************/
 
 let Q = require('q');
-let path = require('path');
-
-/******************************************************************************/
-
-function runInChromeLocally(argv) {
-  let scope = {};
-
-  let httpServer = require('http-server');
-  let portfinder = require('portfinder');
-  let openInChrome = require('../util/open-in-chrome');
-
-  const basePort = 8080;
-  const host = '0.0.0.0';
-
-  let server = httpServer.createServer({
-    root: './',
-    cache: -1,
-    logFn: (request) => {
-      console.log(`[${new Date}] ${request.method} ${request.url}`); // also: request.headers
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    },
-    https: {
-      cert: path.join(__dirname, '..', '..', 'assets', 'certs', 'cert.pem'),
-      key: path.join(__dirname, '..', '..', 'assets', 'certs', 'key.pem'),
-    },
-  });
-
-  portfinder.basePort = basePort;
-
-  return Q.when()
-    .then(() => {
-      return Q.ninvoke(portfinder, "getPort");
-    })
-    .then((port) => {
-      scope.port = port;
-      return Q.ninvoke(server, "listen", port, host);
-    })
-    .then(() => {
-      // Note: there is an npm package called `opener` which may be useful here
-      return openInChrome(`https://${host}:${scope.port}`);
-    })
-    .then(() => {
-      // Right now, we cannot know when the page has finished loading
-      // server.close();
-    });
-}
 
 /******************************************************************************/
 
 exports = module.exports = function run(argv) {
   if (argv._[1] === 'chrome') {
-    return runInChromeLocally(argv);
+    return require('../run').runInChrome();
   }
 
   return Q.reject('Not a valid target');
--- src/run.js
+++ src/run.js
@@ -2,7 +2,59 @@
 
 /******************************************************************************/
 
-exports = module.exports = () => {
-};
+let Q = require('q');
+let path = require('path');
+let fs = require('fs');
+
+/******************************************************************************/
+
+exports.runInChrome = function runInChrome() {
+  let scope = {};
+
+  let httpServer = require('http-server');
+  let portfinder = require('portfinder');
+  let openInChrome = require('./util/open-in-chrome');
+
+  const basePort = 8080;
+  const host = 'localhost';
+  const root = fs.existsSync('app') ? './app' : './';
+
+  let server = httpServer.createServer({
+    root: root,
+    cache: -1,
+    logFn: (request) => {
+      console.log(`[${new Date}] ${request.method} ${request.url}`); // also: request.headers
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    },
+    // Note: Apparently you can load a SW from localhost (but not 0.0.0.0 or 127.0.0.1) domain without SSL
+    /*
+    https: {
+      cert: path.join(__dirname, '..', 'assets', 'certs', 'cert.pem'),
+      key: path.join(__dirname, '..', 'assets', 'certs', 'key.pem'),
+    },
+    */
+  });
+
+  portfinder.basePort = basePort;
+
+  return Q.when()
+    .then(() => {
+      return Q.ninvoke(portfinder, "getPort");
+    })
+    .then((port) => {
+      scope.port = port;
+      return Q.ninvoke(server, "listen", port, host);
+    })
+    .then(() => {
+      return openInChrome(`http://${host}:${scope.port}`);
+    })
+    .then(() => {
+      // Right now, we cannot know when the page has finished loading
+      // server.close();
+    });
+}
 
 /******************************************************************************/
