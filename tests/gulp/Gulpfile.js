ba8fcaef7bb31504e3208623443ce4d3b3748d0b
Mark Koudritsky <kamrik@gmail.com>
2015-02-26 17:22:26 -0500

    Add link to Gulpfile, links 'app' to to requested sample dir.

--- tests/gulp/Gulpfile.js
+++ tests/gulp/Gulpfile.js
@@ -5,11 +5,12 @@
 var gulp = require('gulp');
 var path = require('path');
 var del = require('del');
+var fs = require('fs');
 
 /******************************************************************************/
 
 gulp.task('runInChrome', function() {
-  require('sw2cdv').run.chrome('./app');
+    require('sw2cdv').run.chrome('./app');
 });
 
 /******************************************************************************/
@@ -25,9 +26,34 @@ gulp.task('clean', function(cb) {
 
 /******************************************************************************/
 
+gulp.task('link', ['unlink'], function() {
+    var nopt = require('nopt');
+    var args = nopt({'app': String});
+    var app = args.app || 'basic';
+    var appPath = path.join('..', 'samples', 'service-worker', app);
+    if (!fs.existsSync(appPath)) {
+        console.error('App "' + app + '" not found in ../samples/service-worker/');
+        return;
+    }
+    fs.symlinkSync(appPath, 'app');
+});
+
+gulp.task('unlink', function() {
+    if (!fs.existsSync('app'))
+        return;
+    var stats = fs.lstatSync('app');
+    if (!stats.isSymbolicLink()) {
+        console.error('./app is not a symlink');
+        return;
+    }
+    fs.unlinkSync('app');
+});
+
+/******************************************************************************/
+
 gulp.task('buildios', ['clean'], function() {
     var nopt = require('nopt');
-    var args = nopt({'app': String})
+    var args = nopt({'app': String});
     var app = args.app || 'basic';
 
     // Project definitions
