82a9192e80e1ecf290ecdfbbba5dbe0fed72a25b
Mark Koudritsky <kamrik@gmail.com>
2015-02-25 21:01:29 -0500

    Add buildios task to Gulpfile.js

--- tests/gulp/Gulpfile.js
+++ tests/gulp/Gulpfile.js
@@ -3,6 +3,8 @@
 /******************************************************************************/
 
 var gulp = require('gulp');
+var path = require('path');
+var del = require('del');
 
 /******************************************************************************/
 
@@ -17,3 +19,27 @@ gulp.task('default', ['runInChrome'], function() {
 
 /******************************************************************************/
 
+gulp.task('clean', function(cb) {
+    del(['build'], cb);
+});
+
+/******************************************************************************/
+
+gulp.task('buildios', ['clean'], function() {
+    var nopt = require('nopt');
+    var args = nopt({'app': String})
+    var app = args.app || 'basic';
+
+    // Project definitions
+    var prjInfo = {
+        paths: {
+            www: path.resolve('../samples/service-worker/', app),  // looking staring at
+            root: path.resolve('build'),
+        },
+        appName: 'HelloSwApp',
+        swFile: 'service-worker.js',  // Not yet plumbed through, must be service-worker.js
+    };
+
+    var sw2cdv = require('../../main');
+    return sw2cdv.build(prjInfo).done();
+});
