ba629bf3ed7596f3850c4d78d6d7b7437fc6b7dd
Michal Mocny <mmocny@gmail.com>
2015-05-20 15:37:46 -0400

    Bugfix: add missing require

--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 
 /******************************************************************************/
 
+var path = require('path');
+
+/******************************************************************************/
+
 // Note: you cannot use ecma6 features not offered directly by all node version from within this file,
 // because this use of babel (6to5) clobbers require() and only affects future imported scripts.
 require("babel/register")({
