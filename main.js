40249b470de32652dbf5e4e819116706c216762d
Michal Mocny <mmocny@gmail.com>
2015-05-20 15:36:13 -0400

    Add support for babel in global install

--- main.js
+++ main.js
@@ -6,7 +6,10 @@
 
 // Note: you cannot use ecma6 features not offered directly by all node version from within this file,
 // because this use of babel (6to5) clobbers require() and only affects future imported scripts.
-require("babel/register");
+require("babel/register")({
+    ignore: false,
+    only: new RegExp(path.join(__dirname, 'src'))
+});
 
 /******************************************************************************/
 
